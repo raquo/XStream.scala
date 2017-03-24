9adef2aafca00fc4e5be4b2a8f66658b3def1db0
Nikita Gazarov <nikita@raquo.com>
2017-03-23 18:15:07 -0700

    Make subscription fields public

--- src/main/scala/com/raquo/xstream/Subscription.scala
+++ src/main/scala/com/raquo/xstream/Subscription.scala
@@ -1,13 +1,19 @@
 package com.raquo.xstream
 
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
 
 @js.native
 trait Subscription[T, EE <: Exception] extends js.Object {
 
-  private val _stream: EStream[T, EE] = js.native
+  // @TODO[API] Is there a good reason to keep these private?
+  // @TODO[API] Comparing subscriptions for example is much easier if these are public
 
-  private val _listener: Listener[T, EE] = js.native
+  @JSName("_stream")
+  val stream: EStream[T, EE] = js.native
+
+  @JSName("_listener")
+  val listener: Listener[T, EE] = js.native
 
   def unsubscribe(): Unit = js.native
 }
